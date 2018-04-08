4ddbe2dbe121727c66dbfcef9f62271f7b654efe
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-08 16:56:57 +0900

    挨拶するコンポーネント作った

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
+class Greeting extends Component {
+  render () {
+    return (
+      <Text>Hello, {this.props.name}</Text>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Greeting name='John' />
+        <Greeting name='Michael' />
+        <Greeting name='Catherine' />
       </View>
     )
   }
