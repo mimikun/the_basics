2feddb6dd089f2c7d018f5e632ad898088d3723c
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-08 17:35:39 +0900

    点滅テキストコンポーネントを作った

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
+class Blink extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {isShowingText: true}
+
+    // 毎秒stateを切り替える
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText }
+      })
+    }, 1000)
+  }
+
+  render () {
+    let display = this.state.isShowingText ? this.props.text : ' '
+    return (
+      <Text>{display}</Text>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Blink text='Open up App.js to start working on your app!' />
+        <Blink text='Changes you make will automatically reload.' />
+        <Blink text='Shake your phone to open the developer menu.' />
       </View>
     )
   }
