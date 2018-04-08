65aea3c3245ba7d3d26ffd4a24f3c8eca43f0b3d
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-08 20:14:26 +0900

    flexなレイアウトの四角形を作ってみた

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     )
   }
