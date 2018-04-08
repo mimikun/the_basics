334dc6fb2dbd5c0469fe325200b9efe0101a00da
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-08 16:21:19 +0900

    画像を表示するやつを入れた

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Image, View } from 'react-native'
 
 export default class App extends React.Component {
   render () {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Image source={pic} style={{width: 193, height: 110}} />
       </View>
     )
   }
