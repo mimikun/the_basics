ac88ac5837060ec6a0e97162a33a1bc7547dc649
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-09 11:49:04 +0900

    space-betweenを指定した

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default class App extends Component {
   render () {
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
+      <View style={styles.justify_content}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
