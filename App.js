1d847d3b49e33d408825e86a99bcb365607618b4
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-11 15:41:39 +0900

    ボタンタップするやつを作った

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
 
 export default class App extends Component {
+  _onPressButton () {
+    Alert.alert('あなたはボタンをタップしました！')
+  }
   render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title='Press Me'
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title='Press Me'
+            color='#841584'
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title='This lools great!'
+          />
+          <Button
+            onPress={this._onPressButton}
+            title='OK!'
+            color='#841584'
+          />
+        </View>
       </View>
     )
   }
