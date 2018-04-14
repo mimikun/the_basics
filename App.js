8bb59844ff30c342b3ed091cf2bb2183e4de6970
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-15 05:38:30 +0900

    fetchしてデータ取ってくるやつ作った

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { isLoading: true }
+  }
+
+  componentDidMount () {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   render () {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1, paddingTop: 20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear} </Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
